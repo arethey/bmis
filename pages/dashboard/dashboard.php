42c656d6428afc9320a47c000a46881d2f1e4ab0
arethey <ardiederrayal06@gmail.com>
2022-02-03 00:11:06 +0800

    add blotter count

--- pages/dashboard/dashboard.php
+++ pages/dashboard/dashboard.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 
-    <?php
+<?php
     session_start();
     if(!isset($_SESSION['role']))
     {
@@ -11,142 +11,192 @@
     {
     ob_start();
     include('../head_css.php'); ?>
-    <body class="skin-black">
-        <!-- header logo: style can be found in header.less -->
-        <?php 
+
+<body class="skin-black">
+    <!-- header logo: style can be found in header.less -->
+    <?php 
         
         include "../connection.php";
         ?>
-        <?php include('../header.php'); ?>
+    <?php include('../header.php'); ?>
 
-        <div class="wrapper row-offcanvas row-offcanvas-left">
-            <!-- Left side column. contains the logo and sidebar -->
-            <?php include('../sidebar-left.php'); ?>
+    <div class="wrapper row-offcanvas row-offcanvas-left">
+        <!-- Left side column. contains the logo and sidebar -->
+        <?php include('../sidebar-left.php'); ?>
 
-            <!-- Right side column. Contains the navbar and content of the page -->
-            <aside class="right-side">
-                <!-- Content Header (Page header) -->
-                <section class="content-header">
-                    <h1>
-                        Dashboard
-                    </h1>
-                    
-                </section>
+        <!-- Right side column. Contains the navbar and content of the page -->
+        <aside class="right-side">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                    Dashboard
+                </h1>
 
-                <!-- Main content -->
-                <section class="content">
-                    <div class="row">
-                        <!-- left column -->
-                            <div class="box">
-                                
-                                <div class="col-md-3 col-sm-6 col-xs-12"><br>
-                                  <div class="info-box">
-                                    <a href="../household/household.php"><span class="info-box-icon bg-aqua"><i class="fa fa-home"></i></span></a>
+            </section>
 
-                                    <div class="info-box-content">
-                                      <span class="info-box-text">Total Household</span>
-                                      <span class="info-box-number">
+            <!-- Main content -->
+            <section class="content">
+                <div class="row">
+                    <!-- left column -->
+                    <div class="box">
+                        <div class="col-md-3 col-sm-6 col-xs-12"><br>
+                            <div class="info-box">
+                                <a href="../household/household.php"><span class="info-box-icon bg-aqua"><i
+                                            class="fa fa-home"></i></span></a>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Total UnSolved Blotter</span>
+                                    <span class="info-box-number">
+                                        <?php
+                                            $q = mysqli_query($con,"SELECT * from tblblotter WHERE sStatus='Unsolved'");
+                                            $num_rows = mysqli_num_rows($q);
+                                            echo $num_rows;
+                                        ?>
+                                    </span>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12"><br>
+                            <div class="info-box">
+                                <a href="../household/household.php"><span class="info-box-icon bg-aqua"><i
+                                            class="fa fa-home"></i></span></a>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Total Solved Blotter</span>
+                                    <span class="info-box-number">
+                                        <?php
+                                            $q = mysqli_query($con,"SELECT * from tblblotter WHERE sStatus='Solved'");
+                                            $num_rows = mysqli_num_rows($q);
+                                            echo $num_rows;
+                                        ?>
+                                    </span>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12"><br>
+                            <div class="info-box">
+                                <a href="../household/household.php"><span class="info-box-icon bg-aqua"><i
+                                            class="fa fa-home"></i></span></a>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Total Household</span>
+                                    <span class="info-box-number">
                                         <?php
                                             $q = mysqli_query($con,"SELECT * from tblhousehold");
                                             $num_rows = mysqli_num_rows($q);
                                             echo $num_rows;
                                         ?>
-                                      </span>
-                                    </div>
-                                    <!-- /.info-box-content -->
-                                  </div>
-                                  <!-- /.info-box -->
+                                    </span>
                                 </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
 
-                                <div class="col-md-3 col-sm-6 col-xs-12"><br>
-                                  <div class="info-box">
-                                    <a href="../resident/resident.php"><span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span></a>
+                        <div class="col-md-3 col-sm-6 col-xs-12"><br>
+                            <div class="info-box">
+                                <a href="../resident/resident.php"><span class="info-box-icon bg-aqua"><i
+                                            class="fa fa-users"></i></span></a>
 
-                                    <div class="info-box-content">
-                                      <span class="info-box-text">Total Resident</span>
-                                      <span class="info-box-number">
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Total Resident</span>
+                                    <span class="info-box-number">
                                         <?php
                                             $q = mysqli_query($con,"SELECT * from tblresident");
                                             $num_rows = mysqli_num_rows($q);
                                             echo $num_rows;
                                         ?>
-                                      </span>
-                                    </div>
-                                    <!-- /.info-box-content -->
-                                  </div>
-                                  <!-- /.info-box -->
+                                    </span>
                                 </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
 
-                                <div class="col-md-3 col-sm-6 col-xs-12"><br>
-                                  <div class="info-box">
-                                    <a href="../clearance/clearance.php"><span class="info-box-icon bg-aqua"><i class="fa fa-file"></i></span></a>
+                        <div class="col-md-3 col-sm-6 col-xs-12"><br>
+                            <div class="info-box">
+                                <a href="../clearance/clearance.php"><span class="info-box-icon bg-aqua"><i
+                                            class="fa fa-file"></i></span></a>
 
-                                    <div class="info-box-content">
-                                      <span class="info-box-text">Clearance Issued</span>
-                                      <span class="info-box-number">
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Clearance Issued</span>
+                                    <span class="info-box-number">
                                         <?php
                                             $q = mysqli_query($con,"SELECT * from tblclearance where status = 'Approved' ");
                                             $num_rows = mysqli_num_rows($q);
                                             echo $num_rows;
                                         ?>
-                                      </span>
-                                    </div>
-                                    <!-- /.info-box-content -->
-                                  </div>
-                                  <!-- /.info-box -->
+                                    </span>
                                 </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
 
-                                <div class="col-md-3 col-sm-6 col-xs-12"><br>
-                                  <div class="info-box">
-                                    <a href="../permit/permit.php"><span class="info-box-icon bg-aqua"><i class="fa fa-file"></i></span></a>
+                        <div class="col-md-3 col-sm-6 col-xs-12"><br>
+                            <div class="info-box">
+                                <a href="../permit/permit.php"><span class="info-box-icon bg-aqua"><i
+                                            class="fa fa-file"></i></span></a>
 
-                                    <div class="info-box-content">
-                                      <span class="info-box-text">Permit Issued</span>
-                                      <span class="info-box-number">
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Permit Issued</span>
+                                    <span class="info-box-number">
                                         <?php
                                             $q = mysqli_query($con,"SELECT * from tblpermit where status = 'Approved' ");
                                             $num_rows = mysqli_num_rows($q);
                                             echo $num_rows;
                                         ?>
-                                      </span>
-                                    </div>
-                                    <!-- /.info-box-content -->
-                                  </div>
-                                  <!-- /.info-box -->
+                                    </span>
                                 </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
 
-                                <div class="col-md-3 col-sm-6 col-xs-12"><br>
-                                  <div class="info-box">
-                                    <a href="../blotter/blotter.php"><span class="info-box-icon bg-aqua"><i class="fa fa-user"></i></span></a>
+                        <div class="col-md-3 col-sm-6 col-xs-12"><br>
+                            <div class="info-box">
+                                <a href="../blotter/blotter.php"><span class="info-box-icon bg-aqua"><i
+                                            class="fa fa-user"></i></span></a>
 
-                                    <div class="info-box-content">
-                                      <span class="info-box-text">Blotter Issued</span>
-                                      <span class="info-box-number">
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Blotter Issued</span>
+                                    <span class="info-box-number">
                                         <?php
                                             $q = mysqli_query($con,"SELECT * from tblblotter");
                                             $num_rows = mysqli_num_rows($q);
                                             echo $num_rows;
                                         ?>
-                                      </span>
-                                    </div>
-                                    <!-- /.info-box-content -->
-                                  </div>
-                                  <!-- /.info-box -->
+                                    </span>
                                 </div>
-                            </div><!-- /.box -->
-                    </div>   <!-- /.row -->
-                </section><!-- /.content -->
-            </aside><!-- /.right-side -->
-        </div><!-- ./wrapper -->
-        <!-- jQuery 2.0.2 -->
-        <?php }
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
+                    </div><!-- /.box -->
+                </div> <!-- /.row -->
+            </section><!-- /.content -->
+        </aside><!-- /.right-side -->
+    </div><!-- ./wrapper -->
+    <!-- jQuery 2.0.2 -->
+    <?php }
         include "../footer.php"; ?>
-<script type="text/javascript">
+    <script type="text/javascript">
     $(function() {
         $("#table").dataTable({
-           "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0,5 ] } ],"aaSorting": []
+            "aoColumnDefs": [{
+                "bSortable": false,
+                "aTargets": [0, 5]
+            }],
+            "aaSorting": []
         });
     });
-</script>
-    </body>
+    </script>
+</body>
+
 </html>
